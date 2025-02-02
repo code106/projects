e8ae92ec9a75f62c5d8d0c1e26d4cb86fe8e3498
code106 <147623605+code106@users.noreply.github.com>
2025-02-02 22:12:20 +0530

    Create Curl to get token.sh

---
+++ Curl to get token.sh
@@ -0,0 +1,4 @@
+String curlCommand = "curl -X POST \"https://your-api-endpoint.com/token\" "
+                   + "-H \"Content-Type: application/json\" "
+                   + "-u \"your_client_id:your_client_secret\" "
+                   + "-d '{\"grant_type\": \"client_credentials\"}'";
