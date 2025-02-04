1abe911f969eed3c06d308f00f30d47c23405d0c
code106 <147623605+code106@users.noreply.github.com>
2025-02-04 18:32:59 +0530

    Update Curl to get token.sh

--- Curl to get token.sh
+++ Curl to get token.sh
@@ -9,3 +9,10 @@ curl -X POST "https://vpce-02762b.amazonaws.com/prod/api/abc" \
      -H "Content-Type: application/json" \
      -d '["T1111111", "T2406714"]'
      
+String curlCommand = "curl -X POST \"https://vpce-02762b.amazonaws.com/prod/api/abc\" "
+                   + "-H \"Authorization: Bearer token\" "
+                   + "-H \"x-api-key: aaaa\" "
+                   + "-H \"x-apigw-api-id: bbbb\" "
+                   + "-H \"Content-Type: application/json\" "
+                   + "-d '[\"T1111111\", \"T2406714\"]'";
+                   
