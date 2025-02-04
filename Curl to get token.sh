e099df05c2b766c483746e2b54fdf64e0bd76028
code106 <147623605+code106@users.noreply.github.com>
2025-02-04 18:22:36 +0530

    Update Curl to get token.sh

--- Curl to get token.sh
+++ Curl to get token.sh
@@ -2,3 +2,10 @@ String curlCommand = "curl -X POST \"https://your-api-endpoint.com/token\" "
                    + "-H \"Content-Type: application/json\" "
                    + "-u \"your_client_id:your_client_secret\" "
                    + "-d '{\"grant_type\": \"client_credentials\"}'";
+curl -X POST "https://vpce-02762b.amazonaws.com/prod/api/abc" \
+     -H "Authorization: Bearer token" \
+     -H "x-api-key: aaaa" \
+     -H "x-apigw-api-id: bbbb" \
+     -H "Content-Type: application/json" \
+     -d '["T1111111", "T2406714"]'
+     
