2ecd3db0dd24883fb996edf02fffae2efdaa9706
code106 <147623605+code106@users.noreply.github.com>
2025-03-28 23:25:27 +0530

    Update Mouse.ps1

--- Mouse.ps1
+++ Mouse.ps1
@@ -14,3 +14,13 @@ while ($true) {
     [Mouse]::SetCursorPos($pos.X, $pos.Y)      # Moves it back
     Start-Sleep -Seconds 60
 }
+_----++++
+Add-Type -AssemblyName System.Windows.Forms
+
+while ($true) {
+    $pos = [System.Windows.Forms.Cursor]::Position
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($pos.X + 1, $pos.Y)
+    Start-Sleep -Seconds 60
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($pos.X, $pos.Y)
+    Start-Sleep -Seconds 60
+}
